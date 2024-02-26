b7393cf62c07b82faaa8896c463a03e654746947
httpsyaseen <yaseenwalker1@gmail.com>
2024-02-26 23:00:59 +0500

    todo list completed

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Pressable,
+} from "react-native";
+
+import { v4 as uuidv4 } from "react-native-get-random-values";
 
 export default function App() {
   const [goal, setGoal] = useState([]);
@@ -11,7 +20,14 @@ export default function App() {
   }
 
   function handleClickGoal() {
-    setGoal((goals) => [...goals, enteredText]);
+    setGoal((goals) => [
+      ...goals,
+      { text: enteredText, id: Math.random().toString() },
+    ]);
+  }
+
+  function handleDeleteItem(id) {
+    setGoal((goal) => goal.filter((goal) => goal.id !== id));
   }
 
   return (
@@ -25,13 +41,15 @@ export default function App() {
         <Button title="My Goal" onPress={handleClickGoal} />
       </View>
       <View style={styles.list}>
-        {goal.length > 0 ? (
-          goal.map((mygoal, index) => {
-            return <Text key={index}>{mygoal}</Text>;
-          })
-        ) : (
-          <Text>Your Goals Here</Text>
-        )}
+        {goal.map((mygoal, index) => {
+          return (
+            <Pressable onPress={() => handleDeleteItem(mygoal.id)} key={index}>
+              <Text style={styles.item} key={index}>
+                {mygoal.text}
+              </Text>
+            </Pressable>
+          );
+        })}
       </View>
     </View>
   );
@@ -64,5 +82,16 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  item: {
+    backgroundColor: "purple",
+    padding: 4,
+    color: "white",
+    fontSize: 20,
+    marginTop: 4,
+    borderRadius: 10,
+    width: "70%",
+    textAlign: "center",
   },
 });
